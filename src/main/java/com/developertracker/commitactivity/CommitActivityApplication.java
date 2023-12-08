c9b4b7ac05fcd6cf86025ab7383ed48a8de8a29c
TharangaWeer <tharangaweer321@gmail.com>
2023-12-08 08:15:44 +0530

    initial commit

---
+++ src/main/java/com/developertracker/commitactivity/CommitActivityApplication.java
@@ -0,0 +1,13 @@
+package com.developertracker.commitactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitActivityApplication.class, args);
+	}
+
+}
